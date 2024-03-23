e5bc75f003ad53cdd363cb284a9b5832f1b263c1
Gomchiiii <imjayeong0421@gmail.com>
2024-03-23 19:31:21 +0900

    03/23 CA04 func complete need labeling

---
+++ Assets/Scripts/IGD/IGDApp.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using X;
+
+namespace IGD {
+    public class IGDApp : XApp {
+        // fields
+        private IGD3DCameraPerson m3DCameraPerson = null;
+        public IGD3DCameraPerson get3DCameraPerson() {
+            return this.m3DCameraPerson;
+        }
+        private IGD2DCameraPerson m2DCameraPerson = null;
+        public IGD2DCameraPerson get2DCameraPerson() {
+            return this.m2DCameraPerson;
+        }
+        private XScreen mScreen = null;
+        
+        private IGDPenMarkMgr mPenMarkMgr = null;
+        public IGDPenMarkMgr getPenMarkMgr() {
+            return this.mPenMarkMgr;
+        }
+
+        private XScenarioMgr mScenarioMgr = null;
+        public override XScenarioMgr getScenarioMgr() {
+            return this.mScenarioMgr;
+        }
+        private XLogMgr mLogMgr = null;
+        public override XLogMgr getLogMgr() {
+            return this.mLogMgr;
+        }
+        private IGDKeyEventSource mKeyEventSource = null;
+        private IGDMouseEventSource mMouseEventSource = null;
+        private IGDEventListener mEventListener = null;
+        private IGDCursor2D mCursor = null;
+        public IGDCursor2D getCursor() {
+            return this.mCursor;
+        }
+
+        void Start() {
+            // Unity physics options
+            Physics.queriesHitBackfaces = true;
+
+            this.m3DCameraPerson = new IGD3DCameraPerson();
+            this.m2DCameraPerson = new IGD2DCameraPerson();
+            IGDPlotter.set2DCameraPerson(this.m2DCameraPerson);
+            IGDPlotter.set3DCameraPerson(this.m3DCameraPerson);
+            IGDPlotter.set2DConstants();
+            this.mScreen = new XScreen();
+
+            this.mPenMarkMgr = new IGDPenMarkMgr();
+
+            this.mScenarioMgr = new IGDScenarioMgr(this);
+            this.mLogMgr = new XLogMgr();
+            this.mLogMgr.setPrintOn(true);
+
+            this.mKeyEventSource = new IGDKeyEventSource();
+            this.mMouseEventSource = new IGDMouseEventSource();
+            this.mEventListener = new IGDEventListener(this);
+            this.mCursor = new IGDCursor2D(this);
+
+            this.mKeyEventSource.setEventListener(this.mEventListener);
+            this.mMouseEventSource.setEventListener(this.mEventListener);
+
+            // Call the methods you write in IGDPlotter.HW.cs here.
+
+            Vector4[] cps = new Vector4[6];
+            cps[0] = new Vector4(0.2f, 1f, 0.2f, 0f);
+            cps[1] = new Vector4(1f, 1f, 0f, 1f);
+            cps[2] = new Vector4(0.8f, 0.4f, 0.3f, 1f);
+            cps[3] = new Vector4(0.4f, 0.1f, 0.6f, 1f);
+            cps[4] = new Vector4(0f, 0.3f, 0.6f, 1f);
+            cps[5] = new Vector4(0.3f, 0.1f, 1f, 1f);
+
+            //Vector4[] cps = new Vector4[4];
+            //cps[0] = new Vector4(0.01f, 0f, 0.03f, 0f);
+            //cps[1] = new Vector4(0.01f, 0.01f, 0f, 0f);
+            //cps[2] = new Vector4(0.01f, 0.02f, 0.003f, 0f);
+            //cps[3] = new Vector4(0.01f, 0.02f, 0.04f, 0f);
+
+            double[] U = { 0, 0, 0, 2, 3, 3, 4, 4, 4 };
+
+            IGDPlotter.CA4_1(U, 2);
+        }
+            
+        void Update() {
+            this.m2DCameraPerson.update();
+            this.mKeyEventSource.update();
+            this.mMouseEventSource.update();
+            IGDPlotter.updateImageOrientation();
+        }
+    }
+}
---
+++ Assets/Scripts/IGD/IGDPlotter.CA4.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+using XAppObject;
+using XGeom.NURBS;
+
+// Write the code for the assignments in this file.
+// Make sure that each task becomes a separate method. Test the methods by
+// calling them from the Start method of the IGDApp class.
+namespace IGD {
+    public static partial class IGDPlotter {
+        // CA4-1: Write code to calculate the values of B-spline basis functions
+        // of given parameters, and plot the graphs of orders 0, 1, and 2 basis
+        // functions along with the knot vector.
+        // Input: knot vector U, degree p(=0, 1, 2)
+        public static void CA4_1(double[] U, int p) {
+
+            float xMax = Screen.width / 1.5f;
+            float yMax = Screen.height / 1.5f;
+
+            IGDPlotter.drawArrow2D(Vector2.zero, new Vector2(xMax, 0));
+            IGDPlotter.drawArrow2D(Vector2.zero, new Vector2(0, yMax));
+            float unitLength = xMax / 5f;
+
+            //Knot Vector in Fig 2.6
+            double us = U[0];
+            double ue = U[U.Length - 1];
+
+            //Sampling for render 
+            int usSample = (int)(us * (double)unitLength);
+            int ueSample = (int)(ue * (double)unitLength);
+
+            // m = 11, p = given
+            int m = U.Length;
+ 
+
+            //point to render the basis function 
+            List<List<Vector2>> basisFnsPts = new List<List<Vector2>>();
+            for (int i = 0; i < m - p - 1; i++) {
+                basisFnsPts.Add(new List<Vector2>());
+            }
+
+            for (int i = usSample; i <= ueSample; i++) {
+                double[] BasisFnsValues = XBspline.calcBasisFns((double)i / (double)unitLength, p, U);
+                for (int j = 0; j < BasisFnsValues.Length; j++) {
+                    double basisFnsValue = BasisFnsValues[j];
+                    basisFnsPts[j].Add(new Vector2(i, (float)basisFnsValue * unitLength));
+                }
+            }
+            //plot each basis funs in hsb colors 
+            for (int i = 0; i < basisFnsPts.Count; i++) {
+                drawPolyline2D(basisFnsPts[i], 0.7f, Color.HSVToRGB((float)i / (float)basisFnsPts.Count, 1, 1));
+            }
+
+            //label each knot
+            for (int i = 0; i < m; i++) {
+                IGDPlotter.drawDot2D(new Vector2(unitLength * (float)U[i], 0));
+            }
+
+        }
+    }
+
+}
